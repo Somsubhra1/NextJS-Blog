9ae2adc732f0ddc95bc89be898d81323abdf0390
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-27 22:21:24 +0530

    articles listing and card component done

---
+++ components/ArticleItem.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import articleStyles from "../styles/Article.module.css";
+
+const ArticleItem = ({ article }) => {
+  return (
+    // To make dynamic routes use as prop
+    <Link href="/article/[id]" as={`/article/${article.id}`}>
+      <a className={articleStyles.card}>
+        <h3>{article.title} &rarr;</h3>
+        <p>{article.body}</p>
+      </a>
+    </Link>
+  );
+};
+
+export default ArticleItem;
