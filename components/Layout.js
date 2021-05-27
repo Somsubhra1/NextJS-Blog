236c97e5103a0811047911fe5f1d71428541e531
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-27 21:39:28 +0530

    navigation and links done

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,20 @@
 import styles from "../styles/Layout.module.css";
+import Nav from "./Nav";
 
 // Layouts are used to wrap around pages
 
 // children are the components this layout will wrap around
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h2>Hello</h2>
-        {children}
-      </main>
-    </div>
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h2>Hello</h2>
+          {children}
+        </main>
+      </div>
+    </>
   );
 };
 
---
+++ components/Nav.js
@@ -0,0 +1,17 @@
+import navStyles from "../styles/Nav.module.css";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <nav className={navStyles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
